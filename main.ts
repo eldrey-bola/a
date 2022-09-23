40b21627ef17961277bb339f0e1bb48f934b898b
eldrey-bola <114228611+eldrey-bola@users.noreply.github.com>
2022-09-23 08:01:01 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
-
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("bo")
+    basic.pause(3000)
+    basic.showIcon(IconNames.Asleep)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . # # #
+        . . # # #
+        `)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # # . . .
+        `)
+})
+basic.forever(function () {
+	
+})
