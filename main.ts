c2007fa5e02dd71c2c99e42b51f6bb3aaac15ca8
eldrey-bola <114228611+eldrey-bola@users.noreply.github.com>
2022-09-30 08:00:27 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
+let tocar = false
+let P1 = 0
+let P2 = 0
 input.onPinPressed(TouchPin.P0, function () {
+    tocar = false
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
@@ -9,22 +13,30 @@ input.onPinPressed(TouchPin.P0, function () {
     basic.clearScreen()
 })
 input.onPinPressed(TouchPin.P2, function () {
-    basic.showLeds(`
-        . . . # #
-        . . . # #
-        . . . # #
-        . . # # #
-        . . # # #
-        `)
+    if (tocar == false) {
+        tocar = true
+        P1 += 1
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . # #
+            . . # # #
+            . . # # #
+            `)
+    }
 })
 input.onPinPressed(TouchPin.P1, function () {
-    basic.showLeds(`
-        # # # . .
-        # # # . .
-        # # # . .
-        # # . . .
-        # # . . .
-        `)
+    if (tocar == true) {
+        tocar = true
+        P2 += 1
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # # . . .
+            # # . . .
+            # # . . .
+            `)
+    }
 })
 basic.forever(function () {
 	
